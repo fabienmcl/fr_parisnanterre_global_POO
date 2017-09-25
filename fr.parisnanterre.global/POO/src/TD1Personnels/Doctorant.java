0d6b26c047bc190e8669d97041c820c88a86605b
Fabien <31860446+fabienmcl@users.noreply.github.com>
2017-09-25 17:27:14 +0200

    second commit

---
+++ fr.parisnanterre.global/POO/src/TD1Personnels/Doctorant.java
@@ -0,0 +1,8 @@
+package TD1Personnels;
+
+public class Doctorant extends Etudiant {
+    public Doctorant(String nom, String prenom, long identifiant, int bourse) {
+        super(nom, prenom, identifiant, bourse);
+    }
+
+}
---
+++ fr.parisnanterre.global/POO/src/TD1Shapes/Point3D.java
@@ -0,0 +1,48 @@
+package TD1Shapes;
+
+public class Point3D extends Point2D {
+    double z;
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void translate(double a, double b){
+
+    }
+    public boolean isOrigin(){
+        return x==0 && y==0 ? true:false;
+    }
+    public double distance(Point2D autre){
+        return (autre.getY()-this.y)/(autre.getX()-this.x);
+    }
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z=z;
+
+    }
+
+    @Override
+    public void setY(double y) {
+        super.setY(y);
+    }
+
+    @Override
+    public void setX(double x) {
+        super.setX(x);
+    }
+
+    @Override
+    public double getY() {
+        return super.getY();
+    }
+
+    @Override
+    public double getX() {
+        return super.getX();
+    }
+}
