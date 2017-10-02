c5494163a0cb2b1d03505120488ca73485591ed8
Fabien <fabien.michl@gmail.com>
2017-10-02 17:17:03 +0200

    TD2: first test ok

--- fr.parisnanterre.global/fr.p10.miage.rps.model/src/model/RockPaperScissors.java
+++ fr.parisnanterre.global/fr.p10.miage.rps.model/src/model/RockPaperScissors.java
@@ -1,4 +1,10 @@
 package model;
 
 public class RockPaperScissors {
+    public RockPaperScissors() {
+        super();
+    }
+    public Result play(RPSEnum p1, RPSEnum p2){
+        return  Result.LOST;
+    }
 }
--- fr.parisnanterre.global/fr.p10.miage.rps.model/test/model/RockPaperScissorsTest.java
+++ fr.parisnanterre.global/fr.p10.miage.rps.model/test/model/RockPaperScissorsTest.java
@@ -1,7 +1,21 @@
 package model;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class RockPaperScissorsTest {
+    RockPaperScissors rock;
 
+    //initialisation RockPaperScissors
+    @BeforeClass
+    public void init(){
+        rock = new RockPaperScissors();
+    }
+
+    @Test
+    public void testPlay() throws Exception {
+        assertEquals(rock.play(RPSEnum.PAPER,RPSEnum.SCISSORS),Result.LOST);
+    }
 }
