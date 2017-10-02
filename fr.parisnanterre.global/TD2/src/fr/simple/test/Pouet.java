a8352811284322badb512c6b511faaab555f8712
Fabien <fabien.michl@gmail.com>
2017-10-02 16:49:03 +0200

    TD2 first Test

---
+++ fr.parisnanterre.global/TD2/src/fr/simple/test/Pouet.java
@@ -0,0 +1,11 @@
+package fr.simple.test;
+
+public class Pouet {
+
+    public Pouet(){
+        super();
+    }
+    public int add(int x, int y){
+        return x+y;
+    }
+}
---
+++ fr.parisnanterre.global/TD2/test/fr/simple/test/PouetTest.java
@@ -0,0 +1,7 @@
+package fr.simple.test;
+
+import static org.testng.Assert.*;
+
+public class PouetTest {
+
+}
